db36c05d451ae6b9e2eac1430df4452dd680ca12
snguyenhung <hung.nguyen@kellogg.northwestern.edu>
2022-05-13 21:32:00 +0000

    done with class

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,16 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post["author"] = params["post"]["author"]
+    @post["body"] = params["post"]["body"]
+    @post["image"] = params["post"]["image"]
+    @post.save
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources "companies"
   resources "contacts"
   resources "posts"
+  
 end
